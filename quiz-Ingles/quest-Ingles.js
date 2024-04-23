f20e290cb97422b81fdf33a48f9310e6cf518b95
Heitor Zelbato <heitorzelbato@gmail.com>
2024-04-23 17:43:24 -0300

    Criando estrutura de Quiz Ingles

---
+++ quiz-Ingles/quest-Ingles.js
@@ -0,0 +1,63 @@
+let questions = [
+    {
+        numb: 1,
+        question:"Em que ano ocorreu a Revolução Industrial?",
+        answer:"C. 1760",
+        options: [
+            "A. 1740",
+            "B. 1860",
+            "C. 1760",
+            "D. 1670"
+        ]
+    },
+
+    {
+        numb: 2,
+        question:"Na guerra dos 100 anos,quem foi a figura mais conhecida e quanto tempo durou?",
+        answer:"B. 116 anos, Joana d'Arc",
+        options: [
+            "A. 100 anos, Henrique V",
+            "B. 116 anos, Joana d'Arc",
+            "C. 14 anos, Ricardo II",
+            "D. 98 anos, Henrique V"
+        ]
+    },
+
+    {
+        numb: 3,
+        question:"Quando Getúlio Vargas morreu?",
+        answer:"D. 24 de Agosto de 1954",
+        options: [
+            "A. 15 de outubro de 1964",
+            "B. 9 de janeiro de 1949",
+            "C.25 de março de 1955",
+            "D. 24 de Agosto de 1954"
+        ]
+    },
+    {
+        numb: 4,
+        question:"Qual foi o primeiro astronauta que foi ao espaço?",
+        answer: "C. Iuri Gagarin",
+        options: [
+            "A. Neil Armstrong",
+            "B. Michael Collins",
+            "C. Iuri Gagarin",
+            "D. Jakub Procházka"
+        ]
+    },
+
+    {
+        numb: 5,
+        question:"Quem foi o primeiro presidente do Brasil?",
+        answer:"B. Deodoro da Fonsenca",
+        options: [
+            "A. Fernando Collor ",
+            "B. Deodoro da Fonsenca",
+            "C. Luis Inácio Lula da Silva ",
+            "D. Fernando Henrique Cardoso"
+        ]
+    
+    }
+
+]
+
