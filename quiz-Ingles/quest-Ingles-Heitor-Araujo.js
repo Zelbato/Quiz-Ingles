101fad9716ad2ccfad330aeeb9678d6ad18dc349
Heitor Zelbato <heitorzelbato@gmail.com>
2024-04-29 17:02:06 -0300

    Criando questoes de ingles

---
+++ quiz-Ingles/quest-Ingles-Heitor-Araujo.js
@@ -0,0 +1,63 @@
+let questions = [
+    {
+        numb: 1,
+        question:"Quais dessas palavras nos usamos em um aeroporto? ",
+        answer:"C. Airplane, Airport, baggage, fligth ticket and board",
+        options: [
+            "A.  Visa, airport, board, Gate, hand ladder",
+            "B. Gate, airport, airplane, foot and retired",
+            "C. Airplane, Airport, baggage, fligth ticket and board",
+            "D. Check-in, Gate, Visa, hand ladder and pocket watch"
+        ]
+    },
+
+    {
+        numb: 2,
+        question:"Hello Paulo, what would you like to eat tomorrow morning? Esta frase está",
+        answer:"D. Futuro",
+        options: [
+            "A.  Presente",
+            "B. Passado",
+            "C. Ela não expressa tempo.",
+            "D. Futuro"
+        ]
+    },
+
+    {
+        numb: 3,
+        question:"Quais dessas palavras estão todas no passado(simple past)?",
+        answer:"A. Died, Cried, Enjoyed, Invited and Worried ",
+        options: [
+            "A. Died, Cried, Enjoyed, Invited and Worried ",
+            "B. Answered, Died, Awakened, Failed and soon ",
+            "C.Tomorrow, cried, died, enjoyed and worried ",
+            "D. Soon, Tomorrow, cried, failed and enjoyed "
+        ]
+    },
+    {
+        numb: 4,
+        question:"Spring begins on 20 March and ends on either 20 June or 21 June, in the United States, qual é a estação? ",
+        answer: "C. primavera ",
+        options: [
+            "A. outono",
+            "B. verão ",
+            "C. primavera ",
+            "D. inverno "
+        ]
+    },
+
+    {
+        numb: 5,
+        question:"Marie tem um voo marcado para Roma, Itália às 10:30.  Qual é o horário do voo de Marie? ",
+        answer:"A. My flight is at half past ten ",
+        options: [
+            "A .My flight is at half past ten ",
+            "B. My flight is at ten fourty-five ",
+            "C. My flight is at eleven fourty-five ",
+            "D. My flight is at ten fifty-five "
+        ]
+    
+    }
+
+]
+
